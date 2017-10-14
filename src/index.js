7ee13df032029c8ae10961f631d53dcf8b1c7e38
Manikandan Sundararajan <mtiruman@buffalo.edu>
2017-10-13 20:55:42 -0400

    created a basic ajax request

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var lyrics = '';
 var oReq = new XMLHttpRequest();
 oReq.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200){
-		lyrics = this.responseText;
+		var json = JSON.parse(this.responseText);
 	}
 }
 oReq.open("GET", "https://www.azlyrics.com/lyrics/glassanimals/porksoda.html", false);
